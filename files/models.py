8d5292040b2fa5c5a5559fbeb4341d46758033e8
balddrAdmin <boliklevik@gmail.com>
2025-11-17 22:44:27 +0100

    schema for add filemodel + get update

--- files/models.py
+++ files/models.py
@@ -1,16 +1,18 @@
 from sqlalchemy.orm import Mapped, mapped_column
 from database import Base
 
+from typing import Optional
+
 
 class FileModel(Base):
     __tablename__ = 'files'
 
     uuid: Mapped[int] = mapped_column(primary_key=True)
-    filename: Mapped[str] = mapped_column(nullable=False)
-    original_path: Mapped[str] = mapped_column(nullable=False)
-    result_path: Mapped[str] = mapped_column(nullable=False)
-    stats: Mapped[str] = mapped_column(nullable=False)
-    error: Mapped[str] = mapped_column(nullable=True)
+    filename: Mapped[str]
+    original_path: Mapped[str]
+    result_path: Mapped[Optional[str]]
+    stats: Mapped[str] = mapped_column(default='{}')
+    error: Mapped[Optional[str]]
 
     def __str__(self):
         return f'File(uuid={self.uuid};filename={self.filename})'
--- files/schemas.py
+++ files/schemas.py
@@ -1,10 +1,20 @@
 from pydantic import BaseModel, Field
+from typing import Optional
 
 
 class SFileGet(BaseModel):
     uuid: int
     filename: str = Field(..., description='имя файла')
     original_path: str = Field(..., description='путь к необ. файлу')
-    result_path: str = Field(..., description='путь к об. файлу')
+    result_path: Optional[str] = Field(None, description='путь к об. файлу')
     stats: str = Field(..., description='стата файла')
-    error: str = Field(..., description='ошибка(если есть)')
+    error: Optional[str] = Field(None, description='ошибка(если есть)')
+
+
+class SFileAdd(BaseModel):
+    uuid: int
+    filename: str = Field(..., description='имя файла')
+    original_path: str = Field(..., description='путь к необ. файлу')
+    result_path: Optional[str] = Field(None, description='путь к об. файлу')
+    stats: str = Field(..., description='стата файла')
+    error: Optional[str] = Field(None, description='ошибка(если есть)')
