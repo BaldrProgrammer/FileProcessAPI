c4f8b1b03a4671c0e61b05949ddfa3c9a1bfaf43
balddrAdmin <boliklevik@gmail.com>
2025-11-18 22:53:14 +0100

    main.py CORS + router connect

--- funcs.py
+++ funcs.py
@@ -10,3 +10,16 @@ async def csv_process(uuid, filename, uploaded_file):
         await FileDAO.change_status_by_id(uuid, 'done')
     except Exception as e:
         await FileDAO.change_status_by_id(uuid, 'error')
+
+
+async def json_process(uuid, filename, uploaded_file):
+    try:
+        await FileDAO.change_status_by_id(uuid, 'processing')
+        ...
+
+        with open(filename, 'wb') as file:
+            file.write(uploaded_file.file)
+        await FileDAO.change_status_by_id(uuid, 'done')
+    except Exception as e:
+        await FileDAO.change_status_by_id(uuid, 'error')
+
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
-import asyncio
-import uuid
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-from funcs import csv_process
-
-from fastapi import FastAPI, UploadFile
+from files.routers import router as files_router
 
 app = FastAPI()
 
+app.include_router(files_router)
 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins = ['*'],
+    allow_methods = ['*'],
+    allow_headers = ['*']
+)
