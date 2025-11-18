d8f3beb132bb2d8080cff4cf076c7bca7c06531f
balddrAdmin <boliklevik@gmail.com>
2025-11-18 01:46:33 +0100

    filedao +change_status

--- files/dao.py
+++ files/dao.py
@@ -1,6 +1,27 @@
 from dao.base import BaseDAO
+from database import async_session_maker
 from files.models import FileModel
+
+from sqlalchemy import update as update_sqlalchemy
+from sqlalchemy.exc import SQLAlchemyError
 
 
 class FileDAO(BaseDAO):
     model = FileModel
+
+    @classmethod
+    async def change_status_by_id(cls, uuid, new_status):
+        async with async_session_maker() as session:
+            query = (
+                update_sqlalchemy(cls.model)
+                .filter_by(uuid=uuid)
+                .values(status=new_status)
+                .execution_options(synchronize_session='fetch')
+            )
+            await session.execute(query)
+            try:
+                await session.commit()
+            except SQLAlchemyError as e:
+                await session.rollback()
+                raise e
+            return True
