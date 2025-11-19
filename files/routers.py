e5ed6b7cf10bcd390c762a044023ea1dc5ba13fe
balddrAdmin <boliklevik@gmail.com>
2025-11-19 23:30:34 +0100

    router update + get_file

--- files/routers.py
+++ files/routers.py
@@ -3,25 +3,33 @@ import random
 import asyncio
 
 from fastapi import APIRouter, UploadFile
+from fastapi.responses import FileResponse
 
 from funcs import csv_process, json_process
 from files.dao import FileDAO
 from files.schemas import SFileGet
 
-router = APIRouter()
+router = APIRouter(prefix='/files')
 
 
-@router.get("/files/get_info/{fileid}")
+@router.get("/get_info/{fileid}")
 async def get_fileinfo(fileid: int) -> SFileGet:
     file_obj = await FileDAO.find_by_id(fileid)
     return file_obj
 
 
-@router.post("/files/upload")
+@router.get('/get_file')
+async def get_file(fileid: int) -> FileResponse:
+    file_obj = await FileDAO.find_by_id(fileid)
+    return FileResponse(file_obj.to_dict()['original_path'])
+
+
+@router.post("/upload")
 async def uploadfile(uploaded_file: UploadFile) -> dict:
     file_id = random.randint(0, 2147483647)
     file_extension = uploaded_file.filename.split('.')[-1]
-    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../file_storage", file_extension, str(file_id) + uploaded_file.filename)
+    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../file_storage", file_extension,
+                            str(file_id) + uploaded_file.filename)
 
     file_byte = await uploaded_file.read()
 
