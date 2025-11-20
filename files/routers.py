6d7715fab67d851130e4bdc16d0443a7b2d3b9f4
balddrAdmin <boliklevik@gmail.com>
2025-11-20 23:23:00 +0100

    REST route names

--- files/routers.py
+++ files/routers.py
@@ -12,19 +12,19 @@ from files.schemas import SFileGet
 router = APIRouter(prefix='/files')
 
 
-@router.get("/get_info/{fileid}")
+@router.get("/{fileid}/info")
 async def get_fileinfo(fileid: int) -> SFileGet:
     file_obj = await FileDAO.find_by_id(fileid)
     return file_obj
 
 
-@router.get('/get_file/{fileid}')
+@router.get('/{fileid}/content')
 async def get_file(fileid: int) -> FileResponse:
     file_obj = await FileDAO.find_by_id(fileid)
     return FileResponse(file_obj.to_dict()['path'])
 
 
-@router.get('/get_file_streaming/{fileid}')
+@router.get('/{fileid}/stream')
 async def get_file_streaming(fileid: int) -> StreamingResponse:
     def iterfile(filepath: str):
         with open(filepath, 'rb') as file:
@@ -40,21 +40,8 @@ async def get_file_streaming(fileid: int) -> StreamingResponse:
                              media_type=f'text/{filedict['filename'].split('.')[-1]}')
 
 
-@router.post("/upload")
-async def uploadfile(uploaded_file: UploadFile) -> dict:
-    file_id = random.randint(0, 2147483647)
-    filepath = os.path.join(os.path.abspath(os.path.dirname(__file__)), "../file_storage",
-                            str(file_id) + uploaded_file.filename)
-    file_byte = await uploaded_file.read()
-
-    ok = await file_process(file_id, uploaded_file.filename, filepath, file_byte)
-    if ok:
-        return {'ok': True, 'fileid': file_id}
-    return {'ok': False}
-
-
-@router.post("/upload_multiple")
-async def upload_multiple_file(uploaded_files: List[UploadFile]) -> dict:
+@router.post("/")
+async def upload_file(uploaded_files: List[UploadFile]) -> dict:
     file_ids = []
     for uploaded_file in uploaded_files:
         file_id = random.randint(0, 2147483647)
@@ -69,26 +56,14 @@ async def upload_multiple_file(uploaded_files: List[UploadFile]) -> dict:
 
     return {'ok': True, 'fileids': str(file_ids)}
 
-
-@router.delete("/delete/{fileid}")
-async def deletefile(fileid: int) -> dict:
-    file_obj = await FileDAO.find_by_id(fileid)
-    filedict = file_obj.to_dict()
-
-    await FileDAO.delete(id=filedict['id'])
-    os.remove(filedict['path'])
-    return {'ok': True, 'fileid': fileid}
-
-
-@router.delete("/delete/multiple/{fileids}")
-async def deletefile(fileids: str) -> dict:
+@router.delete("/{fileids}")
+async def delete_multiple(fileid: List[str]) -> dict:
     file_ids_return = []
-    ids = fileids.split(',')
-    for fileid in ids:
-        file_obj = await FileDAO.find_by_id(int(fileid))
+    for fid in fileid:
+        file_obj = await FileDAO.find_by_id(int(fid))
         filedict = file_obj.to_dict()
 
         await FileDAO.delete(id=filedict['id'])
         os.remove(filedict['path'])
-        file_ids_return.append(fileid)
+        file_ids_return.append(fid)
     return {'ok': True, 'fileid': str(file_ids_return)}
