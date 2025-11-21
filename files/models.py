219422c75e6c3866ad571a563eeb94d8f5896b11
balddrAdmin <boliklevik@gmail.com>
2025-11-21 23:56:19 +0100

    usermodel to_dict()

--- files/models.py
+++ files/models.py
@@ -40,11 +40,16 @@ class UserModel(Base):
     password: Mapped[str]
     is_admin: Mapped[bool]
 
-
     def __str__(self):
         return f'User(id={self.id};username={self.username})'
-
 
     def __repr__(self):
         return str(self)
 
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'is_admin': self.is_admin
+        }
