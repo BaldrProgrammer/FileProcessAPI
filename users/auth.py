3f131b6ce416456e7e0baab3733733e672b9037c
balddrAdmin <boliklevik@gmail.com>
2025-11-22 02:49:19 +0100

    decode jwt

--- users/auth.py
+++ users/auth.py
@@ -17,9 +17,14 @@ async def verify_password(password: str, hashed_password: str):
     return pwd_context.verify(password, hashed_password)
 
 
-async def create_jwt_token(data: dict):
+async def jwt_encode(data: dict):
     to_encode = data.copy()
     expire_date = datetime.now(timezone.utc) + timedelta(days=30)
     to_encode.update({'exp': expire_date})
     auth_data = get_token_data()
     return jwt.encode(data, key=auth_data['key'], algorithm=auth_data['algorithm'])
+
+
+async def jwt_decode(token: str):
+    auth_data = get_token_data()
+    return jwt.decode(token, key=auth_data['key'])
