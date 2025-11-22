3151847a07860f9416100e94b03f1aa8d14241aa
balddrAdmin <boliklevik@gmail.com>
2025-11-22 02:26:37 +0100

    verify password

--- users/auth.py
+++ users/auth.py
@@ -5,3 +5,7 @@ pwd_context = CryptContext(schemes=['bcrypt'])
 
 async def get_hash_password(password: str):
     return pwd_context.hash(password)
+
+
+async def verify_password(password: str, hashed_password: str):
+    return pwd_context.verify(password, hashed_password)
