d85315b00afa456d616b93115a38b34843107404
balddrAdmin <boliklevik@gmail.com>
2025-11-23 23:54:04 +0100

    usermodel reform

--- users/auth_router.py
+++ users/auth_router.py
@@ -1,12 +1,12 @@
 from fastapi import APIRouter
 from users.dao import UserDAO
-from users.schemas import SUserReg
+from users.schemas import SUserReg, SUserAuth
 
 router = APIRouter(prefix='/auth')
 
 
 @router.post('/register')
-async def user_register(user: SUserReg) -> dict:
+async def register(user: SUserReg) -> dict:
     check = await UserDAO.add(**user.model_dump())
     if check:
         return {'ok': True}
--- users/models.py
+++ users/models.py
@@ -5,8 +5,8 @@ from database import Base
 class UserModel(Base):
     __tablename__ = 'users'
 
-    id: Mapped[int] = mapped_column(primary_key=True)
-    username: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    username: Mapped[str] = mapped_column(unique=True)
     hashed_password: Mapped[str]
     is_admin: Mapped[bool]
 
