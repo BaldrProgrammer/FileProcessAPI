ac6b57d38b47e7506071c97d66da70e7aadfb4ac
balddrAdmin <boliklevik@gmail.com>
2025-11-24 22:50:00 +0100

    user router get_current_user

--- users/routers.py
+++ users/routers.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, Request
 from users.dao import UserDAO
 from users.schemas import SUserGet
+from users.auth import jwt_decode
 
 router = APIRouter(prefix='/users', tags=['/users'])
 
@@ -8,6 +9,13 @@ router = APIRouter(prefix='/users', tags=['/users'])
 @router.get('/')
 async def get_all_users() -> list[SUserGet]:
     return await UserDAO.find_all()
+
+
+@router.get('/current')
+async def get_current_user(response: Request):
+    token = response.cookies.get('access_token')
+    user_id = (await jwt_decode(token))['user_id']
+    return await UserDAO.find_by_id(user_id)
 
 
 @router.get('/{user_id}')
