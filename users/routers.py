552fd18ca63f863232a63e025787b60f14a277e7
balddrAdmin <boliklevik@gmail.com>
2025-11-24 22:41:37 +0100

    user router get_user_by_id

--- users/routers.py
+++ users/routers.py
@@ -8,3 +8,8 @@ router = APIRouter(prefix='/users', tags=['/users'])
 @router.get('/')
 async def get_all_users() -> list[SUserGet]:
     return await UserDAO.find_all()
+
+
+@router.get('/{user_id}')
+async def get_user_by_id(user_id: int) -> SUserGet:
+    return await UserDAO.find_by_id(user_id)
