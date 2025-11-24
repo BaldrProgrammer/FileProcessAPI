5a18459d3b14c866957cb45730ca12fa38d3ea91
balddrAdmin <boliklevik@gmail.com>
2025-11-24 22:40:07 +0100

    user router get_all_users

--- users/routers.py
+++ users/routers.py
@@ -1,3 +1,10 @@
 from fastapi import APIRouter
+from users.dao import UserDAO
+from users.schemas import SUserGet
 
-router = APIRouter(prefix='users', tags=['/users'])
+router = APIRouter(prefix='/users', tags=['/users'])
+
+
+@router.get('/')
+async def get_all_users() -> list[SUserGet]:
+    return await UserDAO.find_all()
