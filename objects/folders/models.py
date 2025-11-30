bf156173368d7e8fa6867c6ccb839a7d239625db
balddrAdmin <boliklevik@gmail.com>
2025-11-30 01:18:47 +0100

    foldermodel

---
+++ objects/folders/models.py
@@ -0,0 +1,25 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from database import Base
+
+
+class FolderModel(Base):
+    __tablename__ = 'folders'
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    path: Mapped[str]
+    stats: Mapped[str] = mapped_column(default='{}')
+
+    def __str__(self):
+        return f'Folder(id={self.id};name={self.name})'
+
+    def __repr__(self):
+        return str(self)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'path': self.path,
+            'stats': self.stats,
+        }
