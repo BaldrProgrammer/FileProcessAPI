4044d6bde3546a3caefb4098b78ef6d02707cbc0
balddrAdmin <boliklevik@gmail.com>
2025-11-30 01:25:26 +0100

    SFolderAdd schema

--- objects/folders/schemas.py
+++ objects/folders/schemas.py
@@ -6,3 +6,9 @@ class SFolderGet(BaseModel):
     name: str = Field(..., description='имя папки')
     path: str = Field(..., description='путь к папке')
     stats: str = Field(..., description='стата папки')
+
+
+class SFolderAdd(BaseModel):
+    id: int
+    name: str = Field(..., description='имя папки')
+    path: str = Field(..., description='путь к папке')
