d4f4482a64abcade7f6f65f49c561b240e10df11
balddrAdmin <boliklevik@gmail.com>
2025-12-01 21:23:13 +0100

    get_folder_content && rmdir --hard fix

--- folders/routers.py
+++ folders/routers.py
@@ -11,35 +11,41 @@ from users.schemas import SUserGet
 router = APIRouter(prefix='/folders', tags=['/folders'])
 
 
-@router.post('/mkdir/{name}')
-async def mkdir(name: str, user: SUserGet = Depends(current_user)) -> dict:
-    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
-    new_instance = SFolderAdd(name=name, path=path)
+@router.get('/content')
+async def get_folder_content(folder_path: str, user: SUserGet = Depends(current_user)) -> dict:
+    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), folder_path)
+    return {'ok': True, 'content': os.listdir(path)}
+
+
+@router.post('/mkdir')
+async def mkdir(folder_path: str, user: SUserGet = Depends(current_user)) -> dict:
+    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), folder_path)
+    new_instance = SFolderAdd(name=folder_path, path=path)
     os.mkdir(path)
     check = await FolderDAO.add(**new_instance.model_dump())
     if check:
-        return {'ok': True, 'name': name, 'path': path}
-    return {'ok': False, 'name': name}
+        return {'ok': True, 'name': folder_path, 'path': path}
+    return {'ok': False, 'name': folder_path}
 
 
 @router.patch('/ren')
-async def rendir(old_name: str, new_name: str, user: SUserGet = Depends(current_user)) -> dict:
-    oldpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), old_name)
-    newpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), new_name)
+async def rendir(old_path: str, new_path: str, user: SUserGet = Depends(current_user)) -> dict:
+    oldpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), old_path)
+    newpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), new_path)
     os.rename(oldpath, newpath)
     await FolderDAO.update({'path': oldpath}, {'path': newpath})
     return {'ok': True, 'oldpath': oldpath, 'newpath': newpath}
 
 
 @router.delete('/')
-async def rmdir(name: str, hard: Optional[bool], user: SUserGet = Depends(current_user)) -> dict:
-    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
+async def rmdir(folder_path: str, hard: Optional[bool], user: SUserGet = Depends(current_user)) -> dict:
+    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), folder_path)
     files_in_dir = os.listdir(path)
-    if files_in_dir and hard:
+    if (not files_in_dir) or hard:
         for file in files_in_dir:
             os.remove(os.path.join(path, file))
     else:
         return {'ok': False, 'detail': 'folder is not empty'}
     os.rmdir(path)
     await FolderDAO.delete(path=path)
-    return {'ok': True, 'name': name, 'path': path}
+    return {'ok': True, 'name': folder_path, 'path': path}
