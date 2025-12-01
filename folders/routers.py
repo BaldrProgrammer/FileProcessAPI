2b5e6ccb39e8f7dbeed149d2c37b990254e1944d
balddrAdmin <boliklevik@gmail.com>
2025-12-01 01:41:55 +0100

    new headers for folder routers

--- folders/routers.py
+++ folders/routers.py
@@ -18,15 +18,7 @@ async def mkdir(name: str, user: SFolderGet = Depends(current_user)) -> dict:
     return {'ok': False, 'name': name}
 
 
-@router.post('/rmdir/{name}')
-async def rmdir(name: str, user: SFolderGet = Depends(current_user)) -> dict:
-    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
-    os.rmdir(path)
-    await FolderDAO.delete(path=path)
-    return {'ok': True, 'name': name, 'path': path}
-
-
-@router.post('/ren')
+@router.patch('/ren')
 async def rmdir(old_name: str, new_name: str, user: SFolderGet = Depends(current_user)) -> dict:
     oldpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), old_name)
     newpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), new_name)
@@ -34,3 +26,10 @@ async def rmdir(old_name: str, new_name: str, user: SFolderGet = Depends(current
     await FolderDAO.update({'path': oldpath}, {'path': newpath})
     return {'ok': True, 'oldpath': oldpath, 'newpath': newpath}
 
+
+@router.delete('/rmdir/{name}')
+async def rmdir(name: str, user: SFolderGet = Depends(current_user)) -> dict:
+    path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
+    os.rmdir(path)
+    await FolderDAO.delete(path=path)
+    return {'ok': True, 'name': name, 'path': path}
