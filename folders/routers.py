202e1f14807823f8620cb1ac13af0625e9e2c35a
balddrAdmin <boliklevik@gmail.com>
2025-12-01 21:05:13 +0100

    folders schema fix

--- folders/routers.py
+++ folders/routers.py
@@ -1,16 +1,18 @@
 from fastapi import APIRouter, Depends
 from folders.dao import FolderDAO
-from folders.schemas import SFolderGet, SFolderAdd
+from folders.schemas import SFolderAdd
 from users.auth import current_user
 
 from typing import Optional
 import os
 
+from users.schemas import SUserGet
+
 router = APIRouter(prefix='/folders', tags=['/folders'])
 
 
 @router.post('/mkdir/{name}')
-async def mkdir(name: str, user: SFolderGet = Depends(current_user)) -> dict:
+async def mkdir(name: str, user: SUserGet = Depends(current_user)) -> dict:
     path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
     new_instance = SFolderAdd(name=name, path=path)
     os.mkdir(path)
@@ -21,7 +23,7 @@ async def mkdir(name: str, user: SFolderGet = Depends(current_user)) -> dict:
 
 
 @router.patch('/ren')
-async def rmdir(old_name: str, new_name: str, user: SFolderGet = Depends(current_user)) -> dict:
+async def rendir(old_name: str, new_name: str, user: SUserGet = Depends(current_user)) -> dict:
     oldpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), old_name)
     newpath = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), new_name)
     os.rename(oldpath, newpath)
@@ -29,8 +31,8 @@ async def rmdir(old_name: str, new_name: str, user: SFolderGet = Depends(current
     return {'ok': True, 'oldpath': oldpath, 'newpath': newpath}
 
 
-@router.delete('/rmdir/{name}')
-async def rmdir(name: str, hard: Optional[bool], user: SFolderGet = Depends(current_user)) -> dict:
+@router.delete('/')
+async def rmdir(name: str, hard: Optional[bool], user: SUserGet = Depends(current_user)) -> dict:
     path = os.path.join(os.path.dirname(__file__), '../file_storage', str(user.id), name)
     files_in_dir = os.listdir(path)
     if files_in_dir and hard:
