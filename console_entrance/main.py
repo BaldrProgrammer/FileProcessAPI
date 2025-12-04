136398930390fc64b05ed1135d1395fa10a68a70
balddrAdmin <boliklevik@gmail.com>
2025-12-04 23:01:11 +0100

    console api login

--- console_entrance/main.py
+++ console_entrance/main.py
@@ -2,12 +2,19 @@ import requests
 import json
 
 cookies = {}
-URL = 'http://localhost:8000/'
+URL = 'http://localhost:8000'
 text_headers = {
     "accept": "application/json",
     "Content-Type": "application/json"
 }
 
-
 while True:
-    pass
+    if not cookies:
+        print('login!')
+        username = input('enter your username: ')
+        password = input('enter your password: ')
+        response = requests.post(URL + '/auth/log',
+                                 json={'username': username, 'password': password},
+                                 headers=text_headers)
+        cookies = response.cookies.get_dict().copy()
+    
