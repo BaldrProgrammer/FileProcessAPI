cf50af0268da71487f247f9d74b3a8340ef79096
root <root@hnirwzxvga.local>
2025-12-19 22:43:52 +0000

    mkdir file_storage

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/administrator/fileapi/venv
+set -gx VIRTUAL_ENV /root/FileProcessAPI/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
